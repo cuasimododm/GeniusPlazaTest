5cc76701d85a60b3324bf02999d2281240de53ea
David Moncayo <>
2019-02-13 09:21:07 -0500

    Create a ListView and populate with simple data from array

--- app/src/main/java/com/example/geniusplazatest/MainActivity.java
+++ app/src/main/java/com/example/geniusplazatest/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.geniusplazatest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String[] users = {"Pedro Picapiedra", "Bilma Picapiedra", "Pablo Marmol"};
+
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, users);
+        ListView listView = (ListView) findViewById(R.id.users_list_view);
+        listView.setAdapter(adapter);
+
     }
 }
