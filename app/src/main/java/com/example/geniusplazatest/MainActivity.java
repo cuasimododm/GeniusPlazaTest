ef619107657d7fc4999e0a5184748d915b664ccb
David Moncayo <>
2019-02-13 10:29:12 -0500

    Last commit imcomplete

--- app/src/main/java/com/example/geniusplazatest/MainActivity.java
+++ app/src/main/java/com/example/geniusplazatest/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import com.android.volley.*;
 
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.geniusplazatest.adapters.UserAdapter;
-import com.example.geniusplazatest.model.ModelUser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,47 +30,52 @@ public class MainActivity extends AppCompatActivity {
 
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        //String url = "https://reqres.in/api/users?delay=3";
         String url = "https://reqres.in/api/users";
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
-                        try{
-                            JSONObject root = new JSONObject(response);
-                            JSONArray data = root.getJSONArray("data");
-                            ArrayList<ModelUser> users = new ArrayList<ModelUser>();
+                    public void onResponse(JSONObject response) {
+                        ParseResponse(response);
 
-                            for (int i = 0; i < data.length(); ++i)
-                            {
-                                JSONObject jsonUser = data.getJSONObject(i);
-
-                                ModelUser user = new ModelUser(
-                                        jsonUser.getString("first_name"),
-                                        jsonUser.getString("last_name"),
-                                        jsonUser.getString("avatar"));
-                                users.add(user);
-                            }
-
-                            ModelUser[] values = new ModelUser[users.size()];
-                            values = users.toArray(values);
-
-                            ListAdapter adapter = new UserAdapter(MainActivity.this, values);
-                            ListView listView = (ListView) findViewById(R.id.users_list_view);
-                            listView.setAdapter(adapter);
-
-                        } catch (JSONException e) {
-                            Log.e("GeniusPlaza: ", e.getMessage());
-                            e.printStackTrace();
-                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError e) {
-                Log.e("GeniusPlaza: ", e.getMessage());
+                Log.e("GeniusPlaza: ", "MainActivity " + e.getMessage());
                 e.printStackTrace();
             }
         });
-        queue.add(stringRequest);
+        queue.add(jsonRequest);
+    }
+
+    private void ParseResponse(JSONObject response)
+    {
+        try{
+            JSONArray data = response.getJSONArray("data");
+            ArrayList<JSONObject> users = new ArrayList<JSONObject>();
+
+            for (int i = 0; i < data.length(); ++i)
+            {
+                JSONObject jsonUser = data.getJSONObject(i);
+                users.add(jsonUser);
+            }
+
+            CreateListView(users);
+
+
+        } catch (JSONException e) {
+            Log.e("GeniusPlaza: ", "MainActivitye " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void CreateListView(ArrayList<JSONObject> users)
+    {
+        JSONObject[] values = new JSONObject[users.size()];
+        values = users.toArray(values);
+
+        ListAdapter adapter = new UserAdapter(MainActivity.this, values);
+        ListView listView = (ListView) findViewById(R.id.users_list_view);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/geniusplazatest/adapters/UserAdapter.java
+++ app/src/main/java/com/example/geniusplazatest/adapters/UserAdapter.java
@@ -1,7 +1,10 @@
 package com.example.geniusplazatest.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +13,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.geniusplazatest.R;
-import com.example.geniusplazatest.model.ModelUser;
+import com.example.geniusplazatest.helpers.AsyncImageDownloader;
 
-public class UserAdapter extends ArrayAdapter<ModelUser> {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    public UserAdapter(@NonNull Context context, @NonNull ModelUser[] values) {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+
+public class UserAdapter extends ArrayAdapter<JSONObject> {
+
+    public UserAdapter(@NonNull Context context, @NonNull JSONObject[] values) {
         super(context, R.layout.row_user_layout, values);
     }
 
@@ -25,13 +35,24 @@ public class UserAdapter extends ArrayAdapter<ModelUser> {
 
         View view = inflater.inflate(R.layout.row_user_layout, parent, false);
 
-        ModelUser user = getItem(position);
+        JSONObject user = getItem(position);
 
-        TextView firstName = (TextView) view.findViewById(R.id.textViewFirstName);
-        firstName.setText(user.getFirstName());
+        try {
+            TextView firstName = (TextView) view.findViewById(R.id.textViewFirstName);
+            firstName.setText(user.getString("first_name"));
 
-        TextView lastName = (TextView) view.findViewById(R.id.textViewLastName);
-        lastName.setText(user.getLastName());
+            TextView lastName = (TextView) view.findViewById(R.id.textViewLastName);
+            lastName.setText(user.getString("last_name"));
+
+            ImageView avatarImage = (ImageView) view.findViewById(R.id.imageViewAvatar);
+            new AsyncImageDownloader(avatarImage).execute(user.getString("avatar"));
+
+
+        } catch (JSONException e) {
+            Log.e("GeniusPlaza: ", "UserAdapter " + e.getMessage());
+            e.printStackTrace();
+        }
+
 
         return view;
     }
